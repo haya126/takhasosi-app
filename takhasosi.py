ec4980ecc353e7195c67426ff161f1de44ad120e
haya126 <150684524+haya126@users.noreply.github.com>
2025-07-19 22:54:52 +0300

    Add files via upload

--- takhasosi.py
+++ takhasosi.py
@@ -5,18 +5,17 @@ from collections import OrderedDict
 
 components.html(
     """
-    <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-H80R2TH37P"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-H80R2TH37P');
-</script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-H80R2TH37P"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-H80R2TH37P');
+    </script>
     """,
     height=0
 )
+
 
 st.set_page_config(page_title="منصه تخصصي", layout="centered")
 st.markdown("""
